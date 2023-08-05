d598ef32c529b7743ed926ece873fcd13dead0e6
Brian Weasner <weasnerb@gmail.com>
2023-08-05 18:28:39 -0400

    Added simple install/uninstall scripts, want to make more robust ones soon

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -s $(pwd)/alacritty ~/.config/alacritty
+ln -s $(pwd)/hypr ~/.config/hypr
+ln -s $(pwd)/rofi ~/.config/rofi
+ln -s $(pwd)/sway ~/.config/sway
+ln -s $(pwd)/wal ~/.config/wal
+ln -s $(pwd)/waybar ~/.config/waybar
+ln -s $(pwd)/wofi ~/.config/wofi
---
+++ uninstall.sh
@@ -0,0 +1,7 @@
+rm ~/.config/alacritty
+rm ~/.config/hypr
+rm ~/.config/rofi
+rm ~/.config/sway
+rm ~/.config/wal
+rm ~/.config/waybar
+rm ~/.config/wofi
