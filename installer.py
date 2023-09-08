45f18a1eb622dffd3f5e86e91332fbe4d66d9dd1
Brian Weasner <weasnerb@gmail.com>
2023-09-08 01:06:00 -0400

    Working on Adding --all and specific config install flags to installer

--- installer.py
+++ installer.py
@@ -17,7 +17,7 @@ def main():
     if len(vars(args)) == 0:
         cli_app.print_help()
     else:
-        args.func(args)
+        args.func(vars(args))
 
 def setupCli():
     """
@@ -26,8 +26,16 @@ def setupCli():
     parser = argparse.ArgumentParser(prog='Dot Installer', description='Linux Dot/Config file installer for weasnerb/linux-dots.')
     
     subparser = parser.add_subparsers(title='Actions')
-    subparser.add_parser('install', help='Install Dot Files.').set_defaults(func=install)
-    subparser.add_parser('uninstall', help='Uninstall Dot Files.').set_defaults(func=uninstall)
+    
+    install_parser = subparser.add_parser('install', help='Install Dot Files.')
+    install_parser.add_argument('config(s)', nargs='*', help='Dot Config(s) you wish to install.')
+    install_parser.add_argument('--all', action='store_true', help='Set flag to install all configs without prompting.')
+    install_parser.set_defaults(func=install)
+
+    uninstall_parser = subparser.add_parser('uninstall', help='Uninstall Dot Files.')
+    uninstall_parser.add_argument('config(s)', nargs='*', help='Dot Config(s) you wish to uninstall.')
+    uninstall_parser.add_argument('--all', action='store_true', help='Set flag to uninstall all configs without prompting.')
+    uninstall_parser.set_defaults(func=uninstall)
     
     return parser
 
@@ -36,13 +44,23 @@ def install(args):
     Main process for installing dot configs
     """
     configs = get_dot_configs_in_path(SRC_DIR)
-    for src_config_path in configs:
-        config_path_relative_to_src = src_config_path.relative_to(SRC_DIR)
-        if prompt_y_n(f"Install {config_path_relative_to_src}?"):
-            installed_config_path = USER_HOME_DIR.joinpath(config_path_relative_to_src)
-            backup_config(installed_config_path)
-            install_config(src_config_path, installed_config_path)
-            print(f"Done")
+    arg_configs = args['config(s)']
+    if len(arg_configs) > 0:
+        config_paths_relative_to_src_as_str = list(map(lambda p: str(p.relative_to(SRC_DIR)), configs))
+        configs_to_install = list(set(config_paths_relative_to_src_as_str) & set(arg_configs))
+        if len(configs_to_install) > 0: 
+            print(f"Should install {', '.join(arg_configs)}")
+            # INSTALL
+        else:
+            print(f"Configs listed ({', '.join(arg_configs)}) are not available for installation")
+    else:
+        for src_config_path in configs:
+            config_path_relative_to_src = src_config_path.relative_to(SRC_DIR)
+            if args['all'] or prompt_y_n(f"Install {config_path_relative_to_src}?"):
+                installed_config_path = USER_HOME_DIR.joinpath(config_path_relative_to_src)
+                backup_config(installed_config_path)
+                install_config(src_config_path, installed_config_path)
+                print("Done")
 
 def uninstall(args):
     """
@@ -52,17 +70,21 @@ def uninstall(args):
     Why? Well, if we ever delete/remove a config from this repo we won't ask to uninstall it as it won't be in the src folder.
     That means that the stuff we deleted from this repo will break the symbolic link we created during install, but the symbolic link won't be deleted.
     """
+    print(args['config(s)'])
+    return
     configs = get_dot_configs_in_path(SRC_DIR)
     for src_config_path in configs:
         config_path_relative_to_src = src_config_path.relative_to(SRC_DIR)
         installed_config_path = USER_HOME_DIR.joinpath(config_path_relative_to_src)
-        if is_config_installed(installed_config_path) and prompt_y_n(f"Uninstall {config_path_relative_to_src}?"):
+        if is_config_installed(installed_config_path) and (args['all'] or prompt_y_n(f"Uninstall {config_path_relative_to_src}?")):
             uninstall_config(installed_config_path)
             restore_backup_config(installed_config_path)
-            print(f"Done")
+            print("Done")
 
 def backup_config(config_path):
-    # Check if config already exists, if so move to path.bak
+    """
+    Check if config already exists, if so move to path.bak
+    """
     if config_path.exists():
         backup_path = get_backup_config_path(config_path)
         # Recursive check if path.bak already exists, if so move path.bak to path.bak.bak
@@ -72,6 +94,9 @@ def backup_config(config_path):
         config_path.rename(backup_path)
 
 def restore_backup_config(config_path):
+    """
+    Restores a config from path.bak to path
+    """
     if config_path.exists():
         print(f"Cannot Restore Backup for {str(config_path)} because {str(config_path)} currently exists.")
         return
@@ -84,17 +109,30 @@ def restore_backup_config(config_path):
             restore_backup_config(backup_path)
         
 def get_backup_config_path(config_path):
+    """
+    Get the Path where a config should be moved to for backup
+    (current_path.bak)
+    """
     BACKUP_SUFFIX = '.bak'
     return config_path.with_suffix(config_path.suffix + BACKUP_SUFFIX)
 
 def install_config(src_config_path, installed_config_path):
+    """
+    Symbolically link the dot config from src_config_path to the installed_config_path
+    """
     print(f"Installing {str(src_config_path.relative_to(SRC_DIR))}")
     installed_config_path.symlink_to(src_config_path)
 
 def is_config_installed(installed_config_path):
+    """
+    Check to see if a config is installed
+    """
     return installed_config_path.exists() and installed_config_path.is_symlink()
 
 def uninstall_config(installed_config_path):
+    """
+    Delete a config from it's installed_config_path if it was a symbolic link (aka installed via this installer.
+    """
     if is_config_installed(installed_config_path):
         print(f"Deleting {str(installed_config_path)}")
         installed_config_path.unlink()
--- src/.config/nvim-kickstart/init.lua
+++ src/.config/nvim-kickstart/init.lua
@@ -1,37 +1,4 @@
---[[
-
-Kickstart.nvim is *not* a distribution.
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
 require('lazy').setup({
-  -- NOTE: First, some plugins that don't require any configuration
-
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
@@ -162,68 +129,9 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
-
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
 }, {})
 
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
-
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
 -- [[ Basic Keymaps ]]
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -239,40 +147,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
-
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
-
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
-
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
