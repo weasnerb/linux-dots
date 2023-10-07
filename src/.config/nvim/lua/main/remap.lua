11850b0ad781705c55522a58d7ec99e48cf14548
Brian Weasner <weasnerb@gmail.com>
2023-10-06 22:58:36 -0400

    Fix Forward/Backward Jumps in ideavim and add tabbing highlight to nvim config

--- src/.config/nvim/lua/main/remap.lua
+++ src/.config/nvim/lua/main/remap.lua
@@ -5,6 +5,10 @@ vim.g.maplocalleader = ' '
 -- Open Nvim File Explorer
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
 
+-- Keep highlighted values when changing indentation
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
 -- Move highlighted lines up or down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -26,7 +30,7 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 -- Yank to system clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
--- Not really sure what capitol "+Y is for??
+-- Yank till end of line to system clipboard
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 -- Delete to void register
